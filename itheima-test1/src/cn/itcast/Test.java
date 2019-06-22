421cbb75b571d735d3ab0e3f87770bee3d7e9e7e
CR <543014543@qq.com>
2019-06-22 19:56:10 +0800

    第一次提交

---
+++ itheima-test1/src/cn/itcast/Test.java
@@ -0,0 +1,12 @@
+package cn.itcast;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello word!!");
+       
+       System.out.println();
+	}
+
+}
